064f9b303fd4fb49ebb151434916685f5aae410d
rajeev <rajeevisukapatla@gmail.com>
2021-06-30 00:28:52 +0530

    third commit

--- lib/consts/supabase_key.dart
+++
@@ -1,3 +0,0 @@
-const String SUPABASE_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMzQxNjY3MiwiZXhwIjoxOTM4OTkyNjcyfQ.BuWwH96TVN4zSyFGoyx15gjIpULE0PEpgJBsdkaT3ck";
-const String SUPABSE_URL = "https://slfjbeyzhklfjdvxoeji.supabase.co";
--- lib/consts/token_id_username.dart
+++
@@ -1,3 +0,0 @@
-const String TOKEN_KEY = "myfosterpass";
-const String USER_ID_KEY = "userIdpass";
-const String USER_NAME_KEY = "userNamepass";
